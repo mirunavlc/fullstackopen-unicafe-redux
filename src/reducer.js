7a37e7b3b18b677f67d95c7ac09af2eb5bec1c71
rzamfiriu <rzamfiriu@barracuda.com>
2022-10-18 16:41:17 +0300

    Ex.6.1 of part6

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,40 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+    case "GOOD":
+      const goodState = {
+        ...state,
+        good: state.good + 1,
+      };
+      return goodState;
+    case "OK":
+      const okState = {
+        ...state,
+        ok: state.ok + 1,
+      };
+      return okState;
+    case "BAD":
+      const badState = {
+        ...state,
+        bad: state.bad + 1,
+      };
+      return badState;
+    case "ZERO":
+      const zeroState = {
+        good: 0,
+        ok: 0,
+        bad: 0,
+      };
+      return zeroState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
